2690ea15ff418fb3cd833ccb4e9b2bd7779b48fc
Elvin Mazwimairi <eemazwi2@gmail.com>
2025-08-13 16:38:51 +0200

    updated links to use Link

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -130,12 +130,17 @@ const Home: React.FC = () => {
 
             <section className="flex flex-col items-center justify-center mt-8 md:mb-4 relative text-center">
               <motion.a
-                href="/contact-profile-card"
                 whileHover={{ scale: 1.05, boxShadow: "0px 4px 20px rgba(0,0,0,0.3)" }}
                 className="inline-flex items-center px-5 py-2 border border-white text-white rounded-lg hover:bg-white hover:text-black transition"
               >
-                <Download className="mr-2" size={18} />
-                My Contact Card
+                <Link
+                  to="/contact-profile-card"
+                  onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} 
+                  target="_blank" rel="noopener noreferrer"
+                >
+                  <Download className="mr-2" size={18} />
+                  My Contact Card
+                </Link>
               </motion.a>
             </section>
 
