4058c0c830f5a6b559b0e26335d12633f9312f00
Elvin Mazwimairi <eemazwi2@gmail.com>
2026-02-15 21:01:18 +0200

    most recent

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 
-import { Switch, Route } from "wouter";
+import { Switch, Route } from "wouter"; 
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
